25aef16b7c3023a78d21e07f303aa88326927e28
asad <coolsk21@yahoo.com>
2022-06-10 09:56:29 +0600

    JSON data to table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import myUsers from './users.json';
+interface USERS {
+  id: Number;
+  first_name: String;
+  last_name: String;
+  email: String;
+  gender: String;
+  ip_address: String;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'jesonTable';
+  users: USERS[] = myUsers;
+
+  constructor() {
+    console.log(this.users);
+  }
 }
